afbe6a72d49c6604d72ec78c7001b4a594bdf528
nsykim <noahsykim@gmail.com>
2024-10-30 16:25:54 -0400

    refactor n to num_conjunction/num_disjunction

--- src/layers.py
+++ src/layers.py
@@ -8,30 +8,34 @@ EPSILON = 1e-10
 INIT_L = 0.0
 
 class Product(torch.autograd.Function):
-    """Tensor product function."""
+    """Custom tensor product function class"""
     @staticmethod
     def forward(ctx, X):
+        """ Performs the forward pass of the custom layer """
         y = (-1. / (-1. + torch.sum(torch.log(X), dim=1)))
         ctx.save_for_backward(X, y)
         return y
 
     @staticmethod
     def backward(ctx, grad_output):
+        """ Computes the gradient of the loss with respect to the input tensors """
         X, y, = ctx.saved_tensors
         grad_input = grad_output.unsqueeze(1) * (y.unsqueeze(1) ** 2 / (X + EPSILON))
         return grad_input
 
 
 class EstimatedProduct(torch.autograd.Function):
-    """Tensor product function with a estimated derivative."""
+    """ Custom tensor product function class with an estimated derivative """
     @staticmethod
     def forward(ctx, X):
+        """ Performs the forward pass of the custom layer """
         y = (-1. / (-1. + torch.sum(torch.log(X), dim=1)))
         ctx.save_for_backward(X, y)
         return y
 
     @staticmethod
     def backward(ctx, grad_output):
+        """ Computes the gradient of the loss with respect to the input tensors """
         X, y, = ctx.saved_tensors
         grad_input = grad_output.unsqueeze(1) * ((-1. / (-1. + torch.log(y.unsqueeze(1) ** 2))) / (X + EPSILON))
         return grad_input
@@ -39,28 +43,43 @@ class EstimatedProduct(torch.autograd.Function):
 
 
 class GradientGraft(torch.autograd.Function):
+    """ Custom gradient grafting class for the RRL """
     @staticmethod
     def forward(ctx, X, Y):
+        """ Performs the forward pass of the layer """
         ctx.save_for_backward(X, Y)
         return X
     
     @staticmethod
     def backward(ctx, grad_output):
+        """ Performs the backward pass for the custom layer """
         return None, grad_output.clone()
 
 class Binarize(torch.autograd.Function):
+    """ Custom binarization using sigmoid function """
     @staticmethod
     def forward(ctx, X):
+        """ Applies a sigmoid activation function to the input tensor and thresholds the result """
         y = torch.sigmoid(X) > 0.5
         return y.to(torch.float)
     
     @staticmethod
     def backward(ctx, grad_output):
+        """ Performs the backward pass for a custom layer """
         X, y = ctx.saved_tensors
         grad_input = grad_output *  y * (1-y)
         return grad_input
 
 class FeatureBinarizer(nn.Module):
+    """
+    Custom layer that binarizes continuous features into multiple bins and combines them with discrete features.
+    Args:
+        - num_bins (int): The number of bins to use for binarizing continuous features.
+        - input_shape (tuple): The shape of the input features.
+        - min_val (torch.Tensor): The minimum value for bin centers. Default is None.
+        - max_val (torch.Tensor): The maximum value for bin centers. Default is None.
+
+    """
     def __init__(self, num_bins, input_shape, min_val=None, max_val=None):
         super(FeatureBinarizer, self).__init__()
         self.num_bins = num_bins
@@ -79,11 +98,19 @@ class FeatureBinarizer(nn.Module):
             self.bin_centers = nn.Parameter(bin_centers, requires_grad=False)
 
     def _initialize_bin_centers(self):
+        """
+        Initializes the bin centers for continuous features.
+
+        If `min_val` and `max_val` are specified, the bin centers are initialized
+        uniformly within the range [`min_val`, `max_val`]. Otherwise, they are
+        initialized using a standard normal distribution.
+        """
         if self.min_val is not None and self.max_val is not None:
             return self.min_val + torch.rand(self.num_bins, self.continuous_feature_count) * (self.max_val - self.min_val)
         return torch.randn(self.num_bins, self.continuous_feature_count)
 
     def forward(self, input_data):
+        """ Forward pass for the custom layer """
         if self.continuous_feature_count > 0:
             discrete_part, continuous_part = input_data[:, :self.input_shape[0]], input_data[:, self.input_shape[0]:]
             continuous_part = continuous_part.unsqueeze(-1)
@@ -96,13 +123,16 @@ class FeatureBinarizer(nn.Module):
 
     @torch.no_grad()
     def binarized_forward(self, input_data):
+        """ Perform a binarized forward pass on the input data """
         return self.forward(input_data)
 
     def clip_weights(self):
+        """ Clips the weights of the bin centers to be within the specified minimum and maximum values """
         if self.continuous_feature_count > 0 and self.min_val is not None and self.max_val is not None:
             self.bin_centers.data = torch.clamp(self.bin_centers.data, self.min_val, self.max_val)
 
     def generate_feature_names(self, feature_names, mean=None, std=None):
+        """ Generates feature names with bin centers and operators """
         feature_labels = feature_names[:self.input_shape[0]]
 
         if self.continuous_feature_count > 0:
@@ -119,40 +149,54 @@ class FeatureBinarizer(nn.Module):
         return feature_labels
 
 class LinearRegressionLayer(nn.Module):
+    """
+    Custom linear regression layer
+    Args:
+        - num_outputs (int): The number of output features.
+        - input_shape (int): The shape of the input features.
+    """
     def __init__(self, num_outputs, input_shape):
+        """ Initializes the LinearRegressionLayer """
         super(LinearRegressionLayer, self).__init__()
         self.num_outputs = num_outputs
         self.input_shape = input_shape
         self.output_dim = num_outputs
-        self.rid2dim = None
-        self.rule2weights = None
+        self.rid2dim = None # rule id to dimension id
+        self.rule2weights = None # rule to weights
         self.layer_type = 'linear'
         
         self.linear = nn.Linear(self.input_shape, self.output_dim)
 
     def forward(self, inputs):
+        """ Perform a forward pass through the layer """
         x = self.linear(inputs)
         return x
 
     @torch.no_grad()
     def binarized_forward(self, inputs):
+        """ Perform a binarized forward pass through the layer """
         return self.forward(inputs)
 
     def clip(self):
+        """ Clip the weights of the linear layer """
         for param in self.linear.parameters():
             param.data.clamp_(-1.0, 1.0)
 
     def compute_l1_norm(self):
+        """ Compute the L1 norm of the weights """
         return torch.norm(self.linear.weight, p=1)
     
     def compute_l2_norm(self):
+        """ Compute the L2 norm of the weights """ 
         return torch.sum(self.linear.weight ** 2)
     
     def calculate_rule_weights(self, prev_layer, skip_connect_layer):
+        """ Calculate the weights of the rules """
         prev_layer = self.conn.prev_layer
         skip_connect_layer = self.conn.skip_from_layer
 
-        always_act_pos = (prev_layer.activation_nodes == prev_layer.forward_tot)
+        # mark skipped layers, then shift IDs and weights to accomodate the skips
+        always_act_pos = (prev_layer.activation_nodes == prev_layer.forward_tot) # always active position
         merged_dimIDs = prev_dimIDs = {k: (-1, v) for k, v in prev_layer.dimIDs.items()}
         if skip_connect_layer is not None:
             shifted_dimIDs = {(k + prev_layer.output_dim): (-2, v) for k, v in skip_connect_layer.dimIDs.items()}
@@ -160,27 +204,40 @@ class LinearRegressionLayer(nn.Module):
             always_act_pos = torch.cat(
                 [always_act_pos, (skip_connect_layer.activation_nodes == skip_connect_layer.forward_tot)])
         
-        Wl, bl = list(self.linear.parameters())
-        bl = torch.sum(Wl.T[always_act_pos], dim=0) + bl
+        # get linear weights and biases
+        Wl, bl = list(self.linear.parameters()) 
+        bl = torch.sum(Wl.T[always_act_pos], dim=0) + bl # add bias for always active nodes
         Wl = Wl.cpu().detach().numpy()
         self.bl = bl.cpu().detach().numpy()
 
-        marked = defaultdict(lambda: defaultdict(float))
-        rid2dim = {}
-        for label_id, wl in enumerate(Wl):
+        marked = defaultdict(lambda: defaultdict(float)) # dict for rule weights
+        rid2dim = {} # dict for rule ids to dims
+        for label_id, wl in enumerate(Wl): # for each label's weights
             for i, w in enumerate(wl):
                 rid = merged_dimIDs[i]
-                if rid == -1 or rid[1] == -1:
+                if rid == -1 or rid[1] == -1: # if the rule id is invalid, skip
                     continue
-                marked[rid][label_id] += w
-                rid2dim[rid] = i % prev_layer.output_dim
+                marked[rid][label_id] += w # add weight to the rule
+                rid2dim[rid] = i % prev_layer.output_dim # map rule id to dimension id
 
-        self.rid2dim = rid2dim
+        self.rid2dim = rid2dim # save rule id to dimension id mapping
+        # sort rules by the maximum absolute value of their weights
         self.rule2weights = sorted(marked.items(), key=lambda x: max(map(abs, x[1].values())), reverse=True)
 
 
 class ConjunctionLayer(nn.Module):
-    def __init__(self, num_conjunctions, input_shape, alpha=0.999, beta=8, gamma=1, stochastic_grad=False):
+    """
+    A custom neural network layer that performs conjunction operations on the input.
+    Args:
+        num_conjunctions (int): The number of conjunctions to be performed.
+        input_shape (int): The shape of the input tensor.
+        alpha (float, optional): A parameter for the novel conjunction operation. Default is 0.999.
+        beta (float, optional): A parameter for the novel conjunction operation. Default is 8.
+        gamma (float, optional): A parameter for the novel conjunction operation. Default is 1.
+    """
+
+    def __init__(self, num_conjunctions, input_shape, alpha=0.999, beta=8, gamma=1):
+        """ Initializes the ConjunctionLayer """
         super(ConjunctionLayer, self).__init__()
         self.num_conjunctions = num_conjunctions
         self.input_shape = input_shape
@@ -196,12 +253,13 @@ class ConjunctionLayer(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs):
+        """ Perform a forward pass through the layer using novel activation functions """
         continuous_output = self.continuous_forward(inputs)
         binarized_output = self.binarized_forward(inputs)
         return GradientGraft.apply(binarized_output, continuous_output)
 
     def continuous_forward(self, inputs):
-
+        """ Perform a continuous forward pass through the layer using novel activation functions """
         inputs = 1.- inputs
         x1 = (1. - 1. / (1. - (inputs * self.alpha) ** self.beta))
         w1 = (1. - 1. / (1. - (self.weights * self.alpha) ** self.beta))
@@ -209,16 +267,27 @@ class ConjunctionLayer(nn.Module):
 
     @torch.no_grad()
     def binarized_forward(self, inputs):
-
+        """ Perform a binarized forward pass through the layer using novel activation functions """
         binary_weights = Binarize.apply(self.weights - THRESHOLD)
         res = (1 - inputs) @ binary_weights
         return torch.where(res > 0, torch.zeros_like(res), torch.ones_like(res))  
 
     def clip_weights(self):
+        """ Clip the weights of the layer """
         self.weights.data.clamp_(INIT_L, 1.0)
 
 class DisjunctionLayer(nn.Module):
-    def __init__(self, num_disjunctions, input_shape, alpha=0.999, beta=8, gamma=1, stochastic_grad=False):
+    """
+    A custom neural network layer that performs disjunction operations on the input.
+    Args:
+        num_disjunctions (int): The number of disjunctions to be performed.
+        input_shape (int): The shape of the input tensor.
+        alpha (float, optional): A parameter for the novel disjunction operation. Default is 0.999.
+        beta (float, optional): A parameter for the novel disjunction operation. Default is 8.
+        gamma (float, optional): A parameter for the novel disjunction operation. Default is 1.
+    """
+    def __init__(self, num_disjunctions, input_shape, alpha=0.999, beta=8, gamma=1):
+        """ Initializes the DisjunctionLayer """
         super(DisjunctionLayer, self).__init__()
         self.num_disjunctions = num_disjunctions
         self.input_shape = input_shape 
@@ -232,61 +301,85 @@ class DisjunctionLayer(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs):
+        """ Perform a forward pass through the layer using novel activation functions """
         continuous_output = self.continuous_forward(inputs)
         binarized_output = self.binarized_forward(inputs)
         return GradientGraft.apply(binarized_output, continuous_output)
 
     def continuous_forward(self, inputs):
+        """ Perform a continuous forward pass through the layer using novel activation functions """
         x1 = (1. - 1. / (1. - (inputs * self.alpha) ** self.beta))
         w1 = (1. - 1. / (1. - (self.weights * self.alpha) ** self.beta))
         return 1. / (1. + x1 @ w1) ** self.gamma
 
     @torch.no_grad()
     def binarized_forward(self, inputs):
-
+        """ Perform a binarized forward pass through the layer using novel activation functions """
         binary_weights = Binarize.apply(self.weights - THRESHOLD)
         res = inputs @ binary_weights
         return torch.where(res > 0, torch.ones_like(res), torch.zeros_like(res))
 
     def clip_weights(self):
+        """ Clip the weights of the layer """
         self.weights.data.clamp_(INIT_L, 1.0)
 
 class OriginalConjunctionLayer(nn.Module):
-    def __init__(self, n, input_shape, stochastic_grad=False):
+    """
+    A PyTorch layer that performs a standard conjunction operation on the input tensor.
+    Args:
+        num_conjunctions (int): The number of conjunctions to be learned.
+        input_shape (int): The shape of the input tensor.
+        stochastic_grad (bool, optional): If True, use EstimatedProduct for stochastic gradient estimation. 
+                                          If False, use Product. Default is False.
+    """
+
+    def __init__(self, num_conjunctions, input_shape, stochastic_grad=False):
+        """ Initializes the OriginalConjunctionLayer """
         super(OriginalConjunctionLayer, self).__init__()
-        self.n = n
+        self.num_conjunctions = num_conjunctions
         self.input_shape = input_shape
-        self.output_dim = self.n
+        self.output_dim = self.num_conjunctions
         self.layer_type = 'conjunction'
 
         self.weights = nn.Parameter(0.5 * torch.rand(self.input_shape, self.n))
         self.prod = EstimatedProduct if stochastic_grad else Product
 
     def forward(self, inputs):
+        """ Perform a forward pass through the layer """
         continuous_output = self.continuous_forward(inputs)
         binarized_output = self.binarized_forward(inputs)
         return GradientGraft.apply(binarized_output, continuous_output)
 
     def continuous_forward(self, inputs):
+        """ Perform a continuous forward pass through the layer """
         return self.prod(1 - (1 - inputs).unsqueeze(-1) * self.weights)
 
     @torch.no_grad()
     def binarized_forward(self, inputs):
+        """ Perform a binarized forward pass through the layer """
         binarized_weights = Binarize.apply(self.weights - THRESHOLD)
         return torch.prod(1 - (1 - inputs).unsqueeze(-1) * binarized_weights, dim=1)
 
     def clip_weights (self):
+        """ Clip the weights of the layer """
         self.weights.data.clamp_(0.0, 1.0)
 
 class OriginalDisjunctionLayer(nn.Module):
-    def __init__(self, n, input_shape, stochastic_grad=False):
+    """ 
+    A PyTorch layer that performs a standard disjunction operation on the input tensor 
+    Args:
+        num_disjunctions (int): The number of disjunctions to be learned.
+        input_shape (int): The shape of the input tensor.
+        stochastic_grad (bool, optional): If True, use EstimatedProduct for stochastic gradient estimation.
+    """
+    def __init__(self, num_disjunctions, input_shape, stochastic_grad=False):
         super(OriginalDisjunctionLayer, self).__init__()
-        self.n = n
+        self.num_disjunctions = num_disjunctions
         self.input_shape = input_shape
-        self.output_dim = n
+        self.output_dim = num_disjunctions
         self.layer_type = 'disjunction'
 
-        self.weights = nn.Parameter(0.5 * torch.rand(self.input_shape, n))
+        self.weights = nn.Parameter(0.5 * torch.rand(self.input_shape, num_disjunctions))
         self.prod = EstimatedProduct if stochastic_grad else Product
 
     def forward(self, inputs):
@@ -400,8 +493,8 @@ class UnionLayer(nn.Module):
             self.conjunction_layer = ConjunctionLayer(num_conjunctions=units, input_shape=input_shape, alpha=alpha, beta=beta, gamma=gamma)
             self.disjunction_layer = DisjunctionLayer(num_disjunctions=units, input_shape=input_shape, alpha=alpha, beta=beta, gamma=gamma)
         else:
-            self.conjunction_layer = OriginalConjunctionLayer(n=units, input_shape=input_shape, stochastic_grad=estimated_grad)
-            self.disjunction_layer = OriginalDisjunctionLayer(n=units, input_shape=input_shape, stochastic_grad=estimated_grad)
+            self.conjunction_layer = OriginalConjunctionLayer(num_conjunctions=units, input_shape=input_shape, stochastic_grad=estimated_grad)
+            self.disjunction_layer = OriginalDisjunctionLayer(num_disjunctions=units, input_shape=input_shape, stochastic_grad=estimated_grad)
 
     def forward(self, input_tensor):
         return torch.cat([self.conjunction_layer(input_tensor), self.disjunction_layer(input_tensor)], dim=1)
